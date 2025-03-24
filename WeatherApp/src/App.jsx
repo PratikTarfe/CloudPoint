85d8e7748f4b8614f613bbd32f757d51f9d6526f
PratikTarfe <pratiktarfe@gmail.com>
2025-03-24 22:51:28 +0530

    added WeatherApp project

---
+++ WeatherApp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Weather from './components/Weather'
+const classass = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default classass
